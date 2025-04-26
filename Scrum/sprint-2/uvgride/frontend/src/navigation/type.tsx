92c0b2fa12d3bd1ed40a636f92c05f86f66b55b3
Jose Ordoñez <js103@MacBook-Pro-de-JPOB.local>
2025-04-26 13:18:03 -0600

    correción en la navegación, creación de Navigation.tsx

--- Scrum/sprint-2/uvgride/frontend/src/navigation/type.tsx
+++ Scrum/sprint-2/uvgride/frontend/src/navigation/type.tsx
@@ -1,53 +1,7 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-import LoginScreen from '../screens/LoginScreen.js';
-import RegisterScreen from '../screens/RegisterScreen.js';
-import HomeScreen from '../screens/HomeScreen.js';
-import ProfileScreen from '../screens/ProfileScreen.js';
-import SettingsScreen from '../screens/SettingsScreen.js';
-
 export type RootStackParamList = {
-  Login : undefined;
-  Register : undefined;
-  Home : undefined;
-  Profile : undefined;
-  Settings : undefined;
-}
-
-const Stack = createNativeStackNavigator();
-
-export default function Navigation() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen
-          name="Login"
-          component={LoginScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Register"
-          component={RegisterScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen 
-          name="Home" 
-          component={HomeScreen} 
-          options={{ title: 'Inicio' }}
-        />
-        <Stack.Screen 
-          name="Profile" 
-          component={ProfileScreen} 
-          options={{ title: 'Perfil' }}
-        />
-        <Stack.Screen 
-          name="Settings" 
-          component={SettingsScreen} 
-          options={{ title: 'Configuración' }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
+  Login: undefined;
+  Register: undefined;
+  Home: undefined;
+  Profile: undefined;
+  Settings: undefined;
+};
