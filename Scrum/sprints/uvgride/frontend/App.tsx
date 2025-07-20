b723e891e9c38f9ccce9c9a2a3776a86444ebdb0
Ikeel04 <adrianricardo.gm@gmail.com>
2025-07-20 17:52:41 -0600

    Colores claros y oscuros agregados a app

--- Scrum/sprints/uvgride/frontend/App.tsx
+++ Scrum/sprints/uvgride/frontend/App.tsx
@@ -3,13 +3,38 @@ import Navigation from './src/navigation/Navigation';
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { UserProvider } from './src/context/UserContext';
+import { ThemeProvider } from './context/ThemeContext';
+import { useTheme } from './context/ThemeContext';
+import { lightColors, darkColors } from './constants/colors';
 
 export default function App() {
   return (
-    <NavigationContainer>
+    <ThemeProvider>
+      <MainApp />
+    </ThemeProvider>
+  );
+}
+
+function MainApp() {
+  const { theme } = useTheme();
+  const colors = theme === 'light' ? lightColors : darkColors;
+
+  return (
+    <NavigationContainer
+    theme={{
+        colors: {
+          background: colors.background,
+          text: colors.text,
+          primary: colors.primary,
+          card: colors.card,
+          border: colors.border,
+        },
+      }}
+    >
+
       <UserProvider>
         <Navigation />
-        <StatusBar style="light" />
+        <StatusBar style={theme === 'dark' ? 'light' : 'dark'} />
       </UserProvider>
     </NavigationContainer>
   );
