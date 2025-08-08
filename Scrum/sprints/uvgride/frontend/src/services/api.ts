68360a0882000e99c82b4b1f7434e6c9ee81c356
JoseOrdonezB <ord231329@uvg.edu.gt>
2025-08-07 18:27:40 -0600

    corrección en la ip

--- Scrum/sprints/uvgride/frontend/src/services/api.ts
+++ Scrum/sprints/uvgride/frontend/src/services/api.ts
@@ -1,1 +1,1 @@
-export const API_URL = 'http://192.168.0.11:3001'; // reemplaza con tu IP local real para probar la app
+export const API_URL = 'http://ip_local_o_servidor:3001'; // reemplaza con tu IP local real para probar la app
