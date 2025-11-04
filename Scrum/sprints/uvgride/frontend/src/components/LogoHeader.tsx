96b72bf9d0a3c56faff8f2146bb16550deb06e64
Alejandro Anton <105880067+Anton17303@users.noreply.github.com>
2025-11-04 13:43:39 -0600

    creacion de logica de logos

---
+++ Scrum/sprints/uvgride/frontend/src/components/LogoHeader.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { View, Image, StyleSheet } from "react-native";
+
+const LogoHeader: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <Image source={require("../assets/Logo1.png")} style={styles.logo} resizeMode="contain" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    alignItems: "flex-start",
+    paddingTop: 12,
+    paddingBottom: 8,
+  },
+  logo: {
+    width: 120,
+    height: 40,
+  },
+});
+
+export default LogoHeader;
